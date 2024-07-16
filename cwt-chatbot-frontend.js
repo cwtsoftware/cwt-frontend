b699ecde9a92c78c835726a662d097b5b5e547b3
moreno1123 <moreno.franjkovic@outlook.com>
2024-07-16 14:55:00 +0200

    update css

--- cwt-chatbot-frontend.js
+++ cwt-chatbot-frontend.js
@@ -83,17 +83,14 @@ document.addEventListener("DOMContentLoaded", function() {
   var svgDelete = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   svgDelete.id = 'cwt-delete';
   svgDelete.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-  svgDelete.setAttribute('fill', 'none');
-  svgDelete.setAttribute('viewBox', '0 0 24 24');
+  svgDelete.setAttribute('fill', '#ffffff');
+  svgDelete.setAttribute('viewBox', '0 0 256 256');
   svgDelete.setAttribute('stroke-width', '1.5');
-  svgDelete.setAttribute('stroke', '#f5f5f5');
-  svgDelete.setAttribute('height', '1.8em');
-  svgDelete.setAttribute('width', '1.8em');
+  svgDelete.setAttribute('height', '20');
+  svgDelete.setAttribute('width', '20');
 
   var pathDelete = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  pathDelete.setAttribute('stroke-linecap', 'round');
-  pathDelete.setAttribute('stroke-linejoin', 'round');
-  pathDelete.setAttribute('d', 'm14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0');
+  pathDelete.setAttribute('d', 'M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z');
 
   svgDelete.appendChild(pathDelete);
   divHeaderRight.appendChild(svgDelete);
@@ -133,8 +130,8 @@ document.addEventListener("DOMContentLoaded", function() {
   svgArrowDown.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
   svgArrowDown.setAttribute('fill', '#FFFFFF');
   svgArrowDown.setAttribute('viewBox', '0 0 256 256');
-  svgArrowDown.setAttribute('width', '22');
-  svgArrowDown.setAttribute('height', '22');
+  svgArrowDown.setAttribute('width', '18');
+  svgArrowDown.setAttribute('height', '18');
   svgArrowDown.setAttribute('stroke', '#ffffff');
   svgArrowDown.setAttribute('stroke-width', '7px');
 
