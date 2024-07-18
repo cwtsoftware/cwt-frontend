76e695b32b4bd9deb6840dbb46c0599c02c3d72f
moreno1123 <moreno.franjkovic@outlook.com>
2024-07-18 14:38:03 +0200

    fix generateUUID

--- cwt-chatbot-frontend.js
+++ cwt-chatbot-frontend.js
@@ -18,6 +18,18 @@ document.addEventListener("DOMContentLoaded", function() {
   fontLinkElement.href = fontLink;
   document.head.appendChild(fontLinkElement);
 
+  function generateUUID() {
+    let d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+      d += performance.now(); // Use high-precision timer if available
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = (d + Math.random()*16)%16 | 0;
+      d = Math.floor(d/16);
+      return (c === 'x' ? r : (r&0x3|0x8)).toString(16);
+    });
+  }
+
   // start creating agent in dom
   var outerDiv = document.createElement('div');
   outerDiv.id = 'cwt-agent';
